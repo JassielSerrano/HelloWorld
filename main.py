dd45a52fa546dc2ab9a2742be642a0df48f3ce6c
jassi <jassi@gmail.com>
2022-03-15 23:17:32 -0700

    for loop in Rolling Dice

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
 
-def rolldice(min,max):
-    while True:
+
+def rolldice(min, max, limit):
+    for e in range(5):
         print("Rolling dice...")
         print(f"Your number : {random.randint(min,max)}")
         choice = input("Do you want to roll the dice again? (y/n)")
         if choice.lower() == 'n':
-            break
+            quit()
+        if choice.lower() != 'y':
+            print("Not(y/n)")
+            quit()
+            #raise Exception
 
-
-rolldice(1,6)
+rolldice(1, 6, 5)
+print('finsihed successfully')
